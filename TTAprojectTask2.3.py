07fe9ce5cd2e334b8c4acb3d5217806a1066c4c2
Ogbai1 <98412628+Ogbai1@users.noreply.github.com>
2022-02-02 23:38:01 +0000

    Add files via upload

---
+++ TTAprojectTask2.3.py
@@ -0,0 +1,30 @@
+def calculation():
+    operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+
+number_1 = int(input(100))
+number_2 = int(input(10))
+if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+
+elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+
+elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+
+elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+
+else:
+        print('You have not typed a valid operator, please run the program again.')
+calculation() 
